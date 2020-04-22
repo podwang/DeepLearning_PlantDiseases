5863fffcdbcdc10eef5af8b7d87d83ca91313511
Pod <podwang0838@gmail.com>
2020-04-21 20:05:16 -0400

    Update ovft_plot.py

--- Scripts/ovft_plot.py
+++ Scripts/ovft_plot.py
@@ -7,6 +7,7 @@ Created on Tue Apr 21 12:33:00 2020
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 # virtual test generation codes
 cols = ['a', 'b', 'c', 'd']
@@ -15,24 +16,32 @@ virtual_rst = pd.DataFrame(index = rows, columns = cols)
 
 for row in virtual_rst.itertuples():
     for col_idx in cols:
-        virtual_rst.at[row[0],col_idx] = row[0] + ord(col_idx)
+        #virtual_rst.at[row[0],col_idx] = (row[0] + ord(col_idx))/150
+        virtual_rst.at[row[0],col_idx] = 0.9 + np.random.normal(loc = 0,
+                                                                 scale = 0.01)
         
 virtual_rst.to_excel('test.xlsx')
 
 df_ovft_study_rst = pd.read_excel('test.xlsx', index_col = 0)
 
-markers = [',','v','^','p','s','x']
+markers = ['o','v','^','p','s','x']
 
+plt.figure(dpi = 150)
+    
 for idx, col_idx in enumerate(df_ovft_study_rst.columns):
-    plt.scatter(x = df_ovft_study_rst.index, 
-                y = df_ovft_study_rst[col_idx],
-                alpha = 0.8, marker = markers[idx],
-                s = 16)
+    plt.plot(df_ovft_study_rst.index, 
+                 df_ovft_study_rst[col_idx],
+                 '-'+ markers[idx],
+                 linewidth = 0.8,
+                 markersize = 6)
+
 # for marker shapes, see https://matplotlib.org/3.2.1/api/markers_api.html#module-matplotlib.markers
 
-plt.title('Overfit Ablation Study')
+plt.title('Overfitting Ablation Study')
 plt.xlabel('Epochs')
-plt.ylabel('Accuracy / %')
+plt.ylabel('Accuracy')
 plt.legend()
+plt.ylim([0.85,1.00])
+plt.figure(dpi = 2000)
 plt.show()
 
